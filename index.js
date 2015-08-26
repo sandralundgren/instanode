00e7b47aa7175978d3e937fbda503c78a47a6e08
Sandra <sandra@sandralundgren.com>
2015-08-26 14:47:43 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// GRAB THE PACKAGES/VARIABLES WE NEED
+// ==================================================
+
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+
+// CONFIGURE THE APP
+// ==================================================
+// tell node where to look for site resources
+
+app.use(express.static(__dirname + '/public'));
+
+// configure instagram app with client_id
+ig.use({
+  client_id: '3beb897fba7241f89d8374fabb316c99',
+  client_secret: 'd80d5dc12e0e41439782b6f04f1ffb0b'
+});
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// configure instagram app with client-id
+// we'll get to this soon
+// SET THE ROUTES
+// ===================================================
+// home page route - popular images
+
+// home page route - popular images
+app.get('/', function(req, res) {
+
+// use the instagram package to get popular media
+  ig.media_popular(function(err, medias, remaining, limit) {
+// render the home page and pass in the popular images
+    res.render('pages/index', { grams: medias });
+      
+  });
+});
+
+// START THE SERVER
+// ==================================================
+app.listen(8080);
+console.log('App started! Look at http://localhost:8080');
